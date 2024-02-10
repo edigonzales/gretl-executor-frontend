f818a2364d68a86b452174e55e654385a00acc87
Stefan Ziegler <edi.gonzales@gmail.com>
2024-02-10 14:48:46 +0100

    add .sdkmanrc

--- src/main/java/ch/so/agi/gretl/Application.java
+++ src/main/java/ch/so/agi/gretl/Application.java
@@ -1,6 +1,6 @@
 package ch.so.agi.gretl;
 
-import ch.so.agi.gretl.data.SamplePersonRepository;
+//import ch.so.agi.gretl.data.SamplePersonRepository;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import javax.sql.DataSource;
@@ -25,18 +25,18 @@ public class Application implements AppShellConfigurator {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-            SqlInitializationProperties properties, SamplePersonRepository repository) {
-        // This bean ensures the database is only initialized when empty
-        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
-            @Override
-            public boolean initializeDatabase() {
-                if (repository.count() == 0L) {
-                    return super.initializeDatabase();
-                }
-                return false;
-            }
-        };
-    }
+//    @Bean
+//    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
+//            SqlInitializationProperties properties, SamplePersonRepository repository) {
+//        // This bean ensures the database is only initialized when empty
+//        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
+//            @Override
+//            public boolean initializeDatabase() {
+//                if (repository.count() == 0L) {
+//                    return super.initializeDatabase();
+//                }
+//                return false;
+//            }
+//        };
+//    }
 }
--- src/main/java/ch/so/agi/gretl/services/SamplePersonService.java
+++ src/main/java/ch/so/agi/gretl/services/SamplePersonService.java
@@ -1,44 +1,44 @@
 package ch.so.agi.gretl.services;
 
-import ch.so.agi.gretl.data.SamplePerson;
-import ch.so.agi.gretl.data.SamplePersonRepository;
+//import ch.so.agi.gretl.data.SamplePerson;
+//import ch.so.agi.gretl.data.SamplePersonRepository;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SamplePersonService {
 
-    private final SamplePersonRepository repository;
-
-    public SamplePersonService(SamplePersonRepository repository) {
-        this.repository = repository;
-    }
-
-    public Optional<SamplePerson> get(Long id) {
-        return repository.findById(id);
-    }
-
-    public SamplePerson update(SamplePerson entity) {
-        return repository.save(entity);
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
-    }
-
-    public Page<SamplePerson> list(Pageable pageable) {
-        return repository.findAll(pageable);
-    }
-
-    public Page<SamplePerson> list(Pageable pageable, Specification<SamplePerson> filter) {
-        return repository.findAll(filter, pageable);
-    }
-
-    public int count() {
-        return (int) repository.count();
-    }
+//    private final SamplePersonRepository repository;
+//
+//    public SamplePersonService(SamplePersonRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public Optional<SamplePerson> get(Long id) {
+//        return repository.findById(id);
+//    }
+//
+//    public SamplePerson update(SamplePerson entity) {
+//        return repository.save(entity);
+//    }
+//
+//    public void delete(Long id) {
+//        repository.deleteById(id);
+//    }
+//
+//    public Page<SamplePerson> list(Pageable pageable) {
+//        return repository.findAll(pageable);
+//    }
+//
+//    public Page<SamplePerson> list(Pageable pageable, Specification<SamplePerson> filter) {
+//        return repository.findAll(filter, pageable);
+//    }
+//
+//    public int count() {
+//        return (int) repository.count();
+//    }
 
 }
